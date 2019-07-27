48410858ee7693439471b0f34b02488584d26d21
alexisvisco <alexis.viscogliosi@outlook.fr>
2019-07-27 20:22:57 +0200

    add: comments to post route

--- packages/client/src/routes/Post.tsx
+++ packages/client/src/routes/Post.tsx
@@ -1,13 +1,16 @@
 import { Heading, HeadingLevel } from 'baseui/heading'
 import * as React from 'react'
+import { useState } from 'react'
 import { Card } from 'baseui/card'
 import { UpVote } from '../components/upvote/UpVote'
-import { StatefulTabs, Tab } from 'baseui/tabs'
 import { StatefulTextarea as Textarea } from 'baseui/textarea'
 import { Button, SIZE } from 'baseui/button'
 import { getRoadMapStateDescriptor, stateList } from '../components/roadmap/RoadMapState'
-import { useState } from 'react'
 import { Select } from 'baseui/select'
+import { Avatar } from 'baseui/avatar'
+import { GoEyeClosed, GoStar } from 'react-icons/go'
+import { Block } from 'baseui/block'
+import { Tag } from 'baseui/tag'
 
 const statusOptions = stateList.map(e => ({
   id: getRoadMapStateDescriptor(e).name, name: getRoadMapStateDescriptor(e).name
@@ -27,6 +30,37 @@ const StatusCheckbox = () => {
       value={ value }
     />
   )
+}
+
+interface IComment {
+  author: string,
+  content: string,
+  isAdmin: boolean,
+  isInternal: boolean
+}
+
+const Comment = ({ author, content, isAdmin, isInternal }: IComment) => {
+  return <div style={ { display: 'flex' } }>
+    <div>
+      <Avatar name={ author }/>
+    </div>
+    <div style={ { display: 'flex', flexDirection: 'column', marginLeft: '20px', marginTop: '8px' } }>
+      <div style={ { display: 'flex' } }>
+        { isAdmin ?
+          <GoStar color="#276ef1" style={ { display: 'block', marginRight: '10px', marginTop: 'auto', marginBottom: 'auto' } }/> : '' }
+        <p style={ { margin: '0' } }>
+          <strong style={ { cursor: 'pointer' } }>{ author }</strong>
+          <small style={ { marginLeft: '10px' } }>19h ago</small>
+          { isInternal ?
+            <div style={ { marginLeft: '10px', display: 'inline' } }><Tag closeable={ false } variant='light'
+                                                                          kind="warning">
+              internal
+            </Tag></div> : '' }
+        </p>
+      </div>
+      <p>{ content }</p>
+    </div>
+  </div>
 }
 
 
@@ -54,24 +88,30 @@ export const Post = () => {
         </div>
 
         <br/>
-
-        <StatefulTabs initialState={ { activeKey: '0' } }
-                      overrides={ { TabBar: { style: { backgroundColor: 'white' } } } }>
-          <Tab title="Public">Blabla</Tab>
-          <Tab title="Internal">Blabla 2</Tab>
-        </StatefulTabs>
-
+        <br/>
         <br/>
 
         <div style={ { display: 'flex', flexDirection: 'column' } }>
           <Textarea
-            placeholder="Detail your feature request"
+            placeholder="Comment on this feature request"
           />
           <br/>
-          <div style={ { marginLeft: 'auto' } }>
-            <Button size={ SIZE.compact }>Add comment</Button>
+          <div style={ { marginLeft: 'auto', display: 'flex' } }>
+            <Button kind="secondary" size={ SIZE.compact } endEnhancer={ <GoEyeClosed/> }>Internal</Button>
+            <Block marginRight={ '10px' }/>
+            <Button size={ SIZE.compact }>Public</Button>
           </div>
         </div>
+        <br/>
+
+        <Comment isInternal={ false } isAdmin author="Alex Tww." content=" amet, consectetur adipisicing elit. Animi aperiam autem dolore
+              excepturi, exercitationem, incidunt iure magni minus molestiae neque numquam odit provident
+              quasi quisquam reiciendis sed sequi suscipit"/>
+
+        <Comment isInternal isAdmin author="Ifff" content=" amet, consectetur adipisicing elit. Animi aperiam autem dolore
+              excepturi, exercitationem, incidunt iure mag   "/>
+
+        <Comment isAdmin={ false } isInternal={ false } author="Rhod Fha" content="@Ifff ok"/>
       </Card>
 
     </HeadingLevel>
